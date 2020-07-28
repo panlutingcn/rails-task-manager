ec04a213e4ed45b3f4a80b69652bb7f880ae3bbe
luting pan <panluting.cn@gmail.com>
2020-07-28 16:51:03 +0800

    Rails tasks manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD
+
+  # READ
+  # read all the tasks
+  get '/tasks', to: "tasks#index"
+  get '/tasks/new', to: "tasks#new", as: :new
+  get '/tasks/:id', to: "tasks#show", as: :task
+
+  post '/tasks', to: "tasks#create"
+
+  # UPDATE
+  get '/tasks/:id/edit', to: "tasks#edit", as: :edit
+  patch '/tasks/:id', to: "tasks#update"
+
+  # DESTROY
+  delete '/tasks/:id', to: "tasks#destroy"
+
 end
